339f007a3da170d684ae7ff53182b7b95399ab53
jeremiahmonte02-rgb <jeremiahmonte02@gmail.com>
2026-06-27 12:59:40 +0800

    config: map options array directly to evaluate DB_SSL_MODE

--- src/config/database.php
+++ src/config/database.php
@@ -60,7 +60,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA') === 'true' ? true : env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => env('DB_SSL_MODE') === 'REQUIRED' ? true : null,
             ]) : [],
         ],
 
